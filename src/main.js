70ca73aeda43ec5c03830fb8d1707c555bff5156
Dylan Kamauoha <dylantkamauoha@gmail.com>
2019-02-15 18:01:35 -0500

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
+  }
+})
 
 new Vue({
   render: h => h(App),
